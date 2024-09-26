b94bd557bcb60615946f48601e35928c69d2df13
nikan2110 <nikan2110@gmail.com>
2024-09-26 11:24:46 +0300

    add main page template

--- constant.py
+++ constant.py
@@ -8,7 +8,7 @@ smtp_port = 587
 smtp_user = "nikan2110isr@gmail.com"
 smtp_password = os.getenv('SMTP_PASSWORD')
 
-html_template = f"""
+news_html_template = f"""
 <!DOCTYPE html>
 <html lang="he">
 
@@ -39,4 +39,64 @@ html_template = f"""
                 <h1 style="margin: 0; font-size: 26px;">פרדס רימונים: רימונים של יולי</h1>
             </td>
         </tr>
+"""
+
+main_page_html_template_begin = """
+<!DOCTYPE html>
+    <html lang="he">
+    <head>
+        <meta charset="UTF-8">
+        <title>Custom Email</title>
+    </head>
+    <body style="margin: 0; padding: 0; background-color: #ffffff; font-family: Arial, sans-serif;">
+
+        <!-- Main container -->
+        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="600" align="center" 
+        style="background-color: #ffffff; margin: 0 auto; border-collapse: collapse;">
+            
+            <!-- Header Image -->
+            <tr>
+                <td style="text-align: center;">
+                    <img src="cid:main_page_header_image" alt="Header Image" width="600" style="display: block;">
+                </td>
+            </tr>
+"""
+
+main_page_html_template_end = """
+ <!-- Footer (Static Pomegranate Image on the left and Signature on the right) -->
+            <tr>
+                <td style="padding: 20px; background-color: #ffffff;">
+                    <table role="presentation" width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                            <!-- Pomegranate Image on the Left -->
+                            <td width="100" style="text-align: left;">
+                                <img src="cid:main_page_footer_image" alt="Footer Image" width="100" 
+                                style="display: block;">
+                            </td>
+                            <!-- Signature on the Right -->
+                            <td style="text-align: right; direction: rtl; font-size: 14px; color: #7a2e2e;">
+                                <span>רות אליעזר<br>מנהלת מחלקת BI</span>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+        </table>
+    </body>
+    </html>
+"""
+
+main_page_content = """שלום רב,
+קצב הזדקנות האוכלוסייה בישראל נמצא במגמת עלייה משמעותית בשנים האחרונות בעקבות מגמות שונות, כאשר המרכזית בהן היא העלייה המבורכת בתוחלת החיים. בשנת 2020, מנו בני ה-65 ומעלה בישראל כ-1.1 מיליון איש ואישה, וההערכות הן כי בשנת 2040 מספרם יכפיל את עצמו.
+אצלנו במאוחדת גדלנו מ 115,000 ב 2020 עד כ 133,000 ב 2024 אזרחים בני ה-65 ולמעלה (גידול האוכלוסייה המזדקנת הוא בכל המגזרים).
+העלייה בתוחלת החיים בישראל מוגדרת כאחד מהצלחות הגדולות של מערכת הבריאות של מדינת ישראל.
+עם זאת, העלייה בתוחלת החיים מלווה לעיתים קרובות בהידרדרות הדרגתית וממושכת בתפקוד ומצב הבריאות והיא מאיימת על איכות החיים בגיל המבוגר .
+כחלק מהשאיפה לצמצם את הפער בין תוחלת החיים ואיכות החיים, הולך וגובר העיסוק בקרב מדינות העולם, בניהן ישראל, במניעת הדרדרות בקרב האוכלוסייה המזדקנת.
+אחד מהאתגרים – הוא ההתמודדות עם מחלת הדמנציה.
+אם שואלים מה זו דמנציה התשובה מתחילה בזה שדמנציה היא מונח גג המשמש לתיאור קבוצה של תסמינים אשר נגרמים כתוצאה מפגיעה בתאי המוח ומשפיעים על תפקודי הזיכרון, על בהירות החשיבה ועל היכולות החברתיות בצורה חמורה מספיק כדי להפריע לתפקוד היומיומי. בניגוד לדעה הרווחת,  דמנציה אינה מהווה חלק בלתי נפרד מהליך ההזדקנות, וחומרתה עשויה להשתנות מאוד מאדם לאדם .
+חשוב לציין שגילוי מוקדם וטיפול יכולים להשפיע באופן משמעותי על הפרוגנוזה ואיכות החיים של החולים. הרפואה המודרנית מפתחת שיטות אבחון וטיפול חדשות שמטרתן להאט את ההתקדמות ולשפר את איכות החיים של אנשים הסובלים מדמנציה. למודעות למצב ולתמיכה בחולים ויקיריהם תפקיד מרכזי במאבק בדמנציה, מה שהופך את העולם סביבנו למקום אכפתי ומודע יותר.
+
+פיתוח מרכזי ביולי – היה שילוב רשם חדש של דמנציה באפליקציה הקיימת "אזרחים וותיקים". האינטגרציה הזו מאפשרת בניית אוכלוסיות יעד לתוכניות התערבות שונות לצוותים רפואיים.
+בנוסף, בדף מש"א, הוספנו נתונים של מקצעות הבריאות (פיזיותרפיסטים, קליניות תקשורת וריפוי בעיסוק). בתחום הלקוחות הוספנו ניתוח מצב שימוש לשירות "פנייה מקוונת " ודוחות חדשים לשב"ן.
 """
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from config import Session, server
-from constant import smtp_user, smtp_password
+from constant import smtp_user, smtp_password, main_page_content, main_page_html_template_begin, \
+    main_page_html_template_end
 from model import News
 import psycopg2
-from constant import html_template
+from constant import news_html_template
 from tools import generate_news_block
 
 session = Session()
@@ -16,7 +17,7 @@ def fetch_pending_news():
     return session.query(News).filter_by(is_news_send=False).all()
 
 
-def generate_email_html(news_items, html_template):
+def generate_news_block_html(news_items, html_template):
     for news_item in news_items:
         news_block = generate_news_block(news_item)
         html_template += news_block
@@ -30,17 +31,17 @@ def generate_email_html(news_items, html_template):
     return html_template
 
 
-def add_header_and_news_image(msg):
+def add_header_and_news_images_to_news_block(msg):
     news_ids = []
-    with open("header.jpg", 'rb') as img:
+    with open("basic_images/header.jpg", 'rb') as img:
         mime_img = MIMEImage(img.read())
         mime_img.add_header('Content-ID', '<header_image>')
         mime_img.add_header('Content-Disposition', 'inline', filename="header")
         msg.attach(mime_img)
 
-    for image_name in os.listdir("./news_image"):
+    for image_name in os.listdir("news_images"):
         if image_name.endswith('.png'):
-            image_path = os.path.join("./news_image", image_name)
+            image_path = os.path.join("news_images", image_name)
             image_id = image_name.split('.')[0]
 
             news_ids.append(image_id)
@@ -54,8 +55,24 @@ def add_header_and_news_image(msg):
     return news_ids
 
 
-def send_email(html_content):
-    html_content_attachment = MIMEText(html_content, 'html')
+def add_header_and_footer_images_to_main_page(msg):
+    with open("basic_images/header_main_page.jpg", 'rb') as img:
+        mime_img = MIMEImage(img.read())
+        mime_img.add_header('Content-ID', '<main_page_header_image>')
+        mime_img.add_header('Content-Disposition', 'inline', filename="main_page_header_image")
+        msg.attach(mime_img)
+
+    with open("basic_images/footer_main_page.png", 'rb') as img:
+        mime_img = MIMEImage(img.read())
+        mime_img.add_header('Content-ID', '<main_page_footer_image>')
+        mime_img.add_header('Content-Disposition', 'inline', filename="main_page_footer_image")
+        msg.attach(mime_img)
+
+
+def send_email(main_page_html, html_content):
+    full_email_html = f"""{main_page_html}{html_content}"""
+
+    html_content_attachment = MIMEText(full_email_html, 'html')
 
     msg = MIMEMultipart()
     msg['Subject'] = "Week news"
@@ -64,7 +81,8 @@ def send_email(html_content):
 
     msg.attach(html_content_attachment)
 
-    news_ids = add_header_and_news_image(msg)
+    add_header_and_footer_images_to_main_page(msg)
+    news_ids = add_header_and_news_images_to_news_block(msg)
 
     server.starttls()
     server.login(smtp_user, smtp_password)
@@ -79,13 +97,33 @@ def update_news_status(news_ids):
     session.commit()
 
 
+def generate_main_page_html(main_page_html_template_begin, main_page_html_template_end, main_page_content):
+    main_page_html = ""
+    main_page_html += main_page_html_template_begin
+    main_page_html += f"""
+    <tr>
+        <td style="padding: 20px; text-align: right; direction: rtl; background-color: #ffffff; color: #000000; 
+        font-size: 16px; line-height: 1.5;">
+            "{main_page_content}"
+        </td>
+    </tr>"""
+    
+    main_page_html += main_page_html_template_end
+
+    return main_page_html
+
+
 if __name__ == '__main__':
     pending_news = fetch_pending_news()
 
     if pending_news:
-        email_html = generate_email_html(pending_news, html_template)
+        main_page_html = generate_main_page_html(main_page_html_template_begin,
+                                                 main_page_html_template_end,
+                                                 main_page_content)
 
-        news_ids = send_email(email_html)
+        news_block_html = generate_news_block_html(pending_news, news_html_template)
+
+        news_ids = send_email(main_page_html,news_block_html)
 
         # update_news_status(news_ids)
 
